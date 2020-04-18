fb9b54826e40b48bada0a4ec110aa7bb553ddf57
Peter Samuels <p_samuels19@outlook.com>
2020-04-18 04:05:23 -0600

    Website
    Cambio en la Web

--- server.js
+++ server.js
@@ -5,18 +5,18 @@ const path = require('path');
 // Crea una Express app
 const app = express();
 
+// Crea un Router para acceder a las direcciones
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  return res.send('Hola estas acá con el COVID-19 ;)');
+});
+
 // Obtiene la ruta del directorio publico donde se encuentran los elementos estaticos (css, js).
 var publicPath = path.resolve(__dirname, 'HTML'); //path.join(__dirname, 'nombre de carpeta'); también puede ser una opción
 
 // Para que los archivos estaticos queden disponibles.
 app.use(express.static(publicPath));
-
-// Crea un Router para acceder a las direcciones
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    return res.send('Hola estas acá con el COVID-19 ;)');
-  });
   
   router.get('/index',function(req,res){
     res.sendFile(path.join(__dirname+'/HTML/index.html'));
